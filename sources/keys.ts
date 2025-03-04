7f41d38549c4dfa3a311a2965fa036aaf67c5ac1
wwk131903 <1319034976@qq.com>
2025-03-04 19:14:14 +0800

    Update keys.ts

--- sources/keys.ts
+++ sources/keys.ts
@@ -1,5 +1,5 @@
 export const keys = {
     groq: process.env.EXPO_PUBLIC_GROQ_API_KEY ?? '',
     ollama: process.env.EXPO_PUBLIC_OLLAMA_API_URL ?? '',
-    openai: process.env.EXPO_PUBLIC_OPENAI_API_KEY ?? '',
+    openai: process.env.EXPO_PUBLIC_OPENAI_API_KEY sk-1jmibig2602rpeothq16lt72fndsl6g6188ssrtec0g28a2c '',
 };
